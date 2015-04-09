73dacc856d3b3ccc4a09504179c6c14ccc7f363b
Eric Chang <echang@sunrunhome.com>
2015-04-09 16:04:43 -0700

    added main.css

---
+++ src/dialog.js
@@ -0,0 +1,25 @@
+var Dialog = React.createClass({
+  render: function() {
+		return (
+			<div className="react-dialog dialog transparent-border full" >
+			<div className="inside">
+				<h2><span>{this.props.text}</span></h2>
+				<div className="buttons">
+		  		<button className="ok-button" onClick={this.okAction}><span>{this.props.okButtonText}</span></button>
+					<button className="cancel-button" onClick={this.cancelAction}><span>{this.props.cancelButtonText}</span></button>
+				</div>
+			</div>
+		</div>
+  	);
+  }
+});
+
+React.renderComponent(
+	    <Dialog
+				text="Do you want to continue?"
+				okButtonText="OK"
+				cancelButtonText="Cancel"
+				okAction={this.okAction}
+			/>,
+    document.body
+);
