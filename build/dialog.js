2689d49ec8d70bb587e78f61c8654cd0bc699be6
Eric Chang <echang@sunrunhome.com>
2015-04-09 16:36:50 -0700

    updated css

--- build/dialog.js
+++ build/dialog.js
@@ -5,8 +5,8 @@ var Dialog = React.createClass({displayName: "Dialog",
 			React.createElement("div", {className: "inside"}, 
 				React.createElement("h2", null, React.createElement("span", null, this.props.text)), 
 				React.createElement("div", {className: "buttons"}, 
-		  		React.createElement("button", {className: "ok-button", onClick: this.okAction}, React.createElement("span", null, this.props.okButtonText)), 
-					React.createElement("button", {className: "cancel-button", onClick: this.cancelAction}, React.createElement("span", null, this.props.cancelButtonText))
+			  	React.createElement("button", {className: "cancel-button", onClick: this.cancelAction}, React.createElement("span", null, this.props.cancelButtonText)), 
+		  		React.createElement("button", {className: "ok-button", onClick: this.okAction}, React.createElement("span", null, this.props.okButtonText))
 				)
 			)
 		)
--- src/dialog.js
+++ src/dialog.js
@@ -5,8 +5,8 @@ var Dialog = React.createClass({
 			<div className="inside">
 				<h2><span>{this.props.text}</span></h2>
 				<div className="buttons">
+			  	<button className="cancel-button" onClick={this.cancelAction}><span>{this.props.cancelButtonText}</span></button>
 		  		<button className="ok-button" onClick={this.okAction}><span>{this.props.okButtonText}</span></button>
-					<button className="cancel-button" onClick={this.cancelAction}><span>{this.props.cancelButtonText}</span></button>
 				</div>
 			</div>
 		</div>
